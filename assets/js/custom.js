4b41ae782f04a153c1cb04f2e92cb28fa57fdc1d
Fotis Athineos <athinaios.fotios@gmail.com>
2015-04-11 19:24:01 +0300

    fixed for mobile view

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,28 @@
+
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+    init_fullpage();
+    small_display();
+    relative_header_position();
 });
+
+function init_fullpage() {
+    $('#fullpage').fullpage({
+        anchors:['intro', 'saltomortale-1', 'saltomortale-2', 'team', 'red-tree'],
+        sectionsColor: ['#F0FFFF', '#000000', '#000000', '#F5F5DC', '#000000'],
+        navigation: true
+    });
+}
+
+function small_display() {
+    if ($(window).width() < '768') {
+        $("p").addClass("small-display-font-main");
+        $("h1").addClass("small-display-font-h1");
+        $("h2").addClass("small-display-font-h2");
+        $("h1").removeClass("mid-top")
+    }
+};
+
+function relative_header_position() {
+    margin = -Math.floor($(window).height()/3)
+    $('[id^="relative_header"]').css("margin-top", margin + "px")
+}
